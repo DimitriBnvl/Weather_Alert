65bf3f92de43519ad5e2503e6421029b8a378b72
Dimitri Bonneville <dimitribonneville1@gmail.com>
2026-04-10 19:08:00 +0200

    Reformat weather conditions and output message

--- main.py
+++ main.py
@@ -17,38 +17,74 @@ weather_parameters = {
 }
 
 def request_weather(params):
+    """
+    Makes a request to the OpenWeatherMap API using provided parameters.
+
+    Args:
+        params (dict): Dictionary of parameters including lat, lon, and appid.
+
+    Returns:
+        dict: The JSON response from the API.
+    """
     response = requests.get(OWM_ENDPOINT, params=params)
     response.raise_for_status()
     data = response.json()
     return data
 
+def get_time_offset(index, interval):
+    """
+    Calculates the hour offset from now for a given forecast index.
+
+    Args:
+        index (int): The index in the forecast list.
+        interval (int): The number of hours between each forecast step.
+
+    Returns:
+        int: The number of hours from the start time.
+    """
+    return index * interval
+
 def detect_precipitation(data):
-    thunderstorm_detected = False
-    drizzle_detected      = False
-    rainfall_detected     = False
-    snowfall_detected     = False
+    """
+    Analyzes the weather data to identify precipitation events.
 
-    for index in range(len(data["list"])):
-        timestep = index * TIMESTEP_INTERVAL
-        weather_condition = data["list"][index]["main"]["temp"]
+    Args:
+        data (dict): The weather forecast data from the OWM API.
 
-        if 200 <= weather_condition < 300:
-            thunderstorm_detected = True
-            thunderstorm_time = timestep
-        if 300 <= weather_condition < 400:
-            drizzle_detected = True
-            drizzle_time = timestep
-        if 500 <= weather_condition < 600:
-            rainfall_detected = True
-            rainfall_time = timestep
-        if 600 <= weather_condition < 700:
-            snowfall_detected = True
-            snowfall_time = timestep
+    Returns:
+        list: A list of dictionaries, each describing a weather event found.
+              Format: [{"condition": str, "hour_offset": int}, ...]
+    """
+    detected_events = []
 
+    for index, item in enumerate(data["list"]):
+        hour_offset = get_time_offset(index, TIMESTEP_INTERVAL)
+        weather_id = item["weather"][0]["id"]
+        
+        condition = None
+        if 200 <= weather_id < 300:
+            condition = "Thunderstorm"
+        elif 300 <= weather_id < 400:
+            condition = "Drizzle"
+        elif 500 <= weather_id < 600:
+            condition = "Rain"
+        elif 600 <= weather_id < 700:
+            condition = "Snow"
+
+        if condition:
+            detected_events.append({
+                "condition": condition,
+                "hour_offset": hour_offset
+            })
+            
+    return detected_events
 
 weather_data = request_weather(weather_parameters)
-detect_precipitation(weather_data)
+events = detect_precipitation(weather_data)
 
-#TODO: Add documentation to functions
-#TODO: Add detection message
-#TODO: index * TIMESTEP_INTERVAL
+if events:
+    print("Precipitation detected:")
+    for event in events:
+        print(f"- {event['condition']} at {event['hour_offset']} hours from now.")
+else:
+    print("No precipitation detected in the forecast period.")
