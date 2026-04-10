8a87972454da7e7e7fedba5caf11f2b7a208628b
Dimitri Bonneville <dimitribonneville1@gmail.com>
2026-04-10 17:49:23 +0200

    Added weather parameters and made request_weather function

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import requests
+
 API_KEY = "87bc5a5b3eb4f9d629def5deb9ed9fb9"
-MY_LAT = 51.507351
-MY_LONG = -0.127758
+OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+MY_LAT = 55.953251
+MY_LONG = -3.188267
+UNITS = "metric"
+TIMESTEPS = 4
+
+weather_parameters = {
+    "lat"  : MY_LAT,
+    "lon"  : MY_LONG,
+    "appid": API_KEY,
+    "units": UNITS,
+    "cnt"  : TIMESTEPS,
+}
+
+def request_weather(params):
+    response = requests.get(OWM_ENDPOINT, params=params)
+    response.raise_for_status()
+    data = response.json()
+    return data
+
+print(request_weather(weather_parameters))
